c21b800519c97036620d725ef4f7dc2f8145fc38
monagano <morihiro-nagano@jslabo.com>
2024-03-02 21:55:31 +0900

    会話履歴ファイルからセッション復元する機能を追加

--- slackbot_jsl/app.py
+++ slackbot_jsl/app.py
@@ -1,19 +1,18 @@
 # coding: utf-8
-import os, json, datetime
+import os, json, datetime, glob
 from openai import OpenAI
 from datetime import datetime
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 
 # slackbotやopenapiのAPIキーはの環境変数に入れておいてね
-client = OpenAI(
-    # This is the default and can be omitted
-    api_key=os.environ.get("OPENAI_API_KEY"),
-)
+# ボットトークンとソケットモードハンドラーを使ってアプリを初期化
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
+app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
 
 
 current_sessions ={}
-
+context_length = 8
 # aiの振る舞いを記載したテキストファイルパスを同フォルダに置いておいてね
 ai_instructions_file_path = 'ai_instructions.md'
 
@@ -21,53 +20,71 @@ ai_instructions_file_path = 'ai_instructions.md'
 ai_instructions:str = "あなたはなんか賢いかんじのChatBotです。"
 
 
-def add_message(user_id, message):
-    # もしそのユーザの地図がまだ描かれていなかったら、地図を新しく作るんだ✏️
-    if user_id not in current_sessions:
-        current_sessions[user_id] = []
-    # そしてそのユーザの地図に、トークオブジェクトの宝石を埋め込むのさ💎✨
-    current_sessions[user_id].append(message)
+def put_messages(user_id, messages):
+    current_sessions[user_id] = messages
+def get_messsages(user_id, message_count):
+    """
+    ユーザーのセッションから全メッセージを取得します。
+    存在しない場合、historyファイルから会話履歴を取得
+    """
+    return current_sessions.get(user_id, get_messages_from_history(user_id, message_count))[:message_count]
 
-def get_message(user_id):
-    if user_id not in current_sessions:
-        return []
-    return current_sessions[user_id]
-# ボットトークンとソケットモードハンドラーを使ってアプリを初期化します
-app = App(token=os.environ.get("SLACK_BOT_TOKEN"))
+def get_messages_from_history(user_id, message_count):
+    """
+    historyファイルから会話履歴を取得
+    """
+    print(os.getcwd())
+    resList = [res['choices'][0]['message'] for res 
+               in load_json_files(glob.glob(f'./slackbot_jsl/history/gpt_response_{user_id}*.json'), message_count // 2, islast=True)]
+    resList = [whitelist_dic(msg, ['role', 'content']) for msg in resList] # resListの各要素についてrole,content以外は除去
+    reqList = [ {"role": "user", "content": f"{req['event']['text']}"} for req 
+               in load_json_files(glob.glob(f'./slackbot_jsl/history/slack_request_{user_id}*.json'), message_count // 2, islast=True)]
+    return [elem for pair in zip(reqList, resList) for elem in pair]
 
-# メンション以外、helloを含む場合。権限付けてないので今は有効じゃない
-@app.message("hello")
-def message_hello(body, say):
-    # イベントがトリガーされたチャンネルへ say() でメッセージを送信します
-    handle_message(body, say)
+def whitelist_dic(dic:dict, whitelist:list[str]) -> dict:
+    return {k: dic[k] for k in whitelist if k in dic}
 
-# メンション
 @app.event("app_mention")
 def message_mention(body, say):
+    """
+    アプリがメンションされた時の処理。(DMはDM側で処理)
+    """
     handle_message(body, say)
 
-# DMはメンションついててもついてなくてもこれ
 @app.event("message")
 def handle_message_events(body, say):
+    """
+    DMを含むメッセージ受信時の処理。
+    """
     handle_message(body, say)
 
 
 def handle_message(body, say):
-    '''
-    一番大事なコードだよ！
-    '''
-    print(json.dumps(body,indent=2))
+    """
+    Slackからのメッセージを処理し、OpenAI GPTへの問い合わせとその応答を行います。
+    """
+    if 'user' not in body['event']:
+        # TODO キャラクター設定ごとに用意
+        say(f"<@{body['event']['message']['user']}>\r\n普通のチャットしか応答できないんだ！ごめんね！\r\n(チャットの編集やファイルアップロードにはまだ対応してないよ！)")
+        return
     slack_user_id = body['event']['user']
-    write_text_to_file_with_timestamp(f"slack_request_{slack_user_id}.json", json.dumps(body,indent=2), True)
-    resp = send_text_to_gpt(body["event"]["text"], slack_user_id)
+    user_message = body["event"]["text"]
+    print(json.dumps(body,indent=2))
+    resp = send_text_to_gpt(user_message, slack_user_id)
     print(resp)
-    write_text_to_file_with_timestamp(f"gpt_response_{slack_user_id}.json", resp.model_dump_json(indent=2), True)
+    isSucced = resp.choices[0].finish_reason == "stop"
+    write_text_to_file_with_timestamp(
+        f"./slackbot_jsl/history/{('' if isSucced else 'error_')}slack_request_{slack_user_id}.json", 
+        json.dumps(body,indent=2), True)
+    write_text_to_file_with_timestamp(
+        f"./slackbot_jsl/history/{('' if isSucced else 'error_')}gpt_response_{slack_user_id}.json", 
+        resp.model_dump_json(indent=2), True)
 
     reply = resp.choices[0].message.content.strip()
     say(f"<@{slack_user_id}>\r\n{reply}")
 
 
-def write_text_to_file_with_timestamp(file_path, text, timestamp=False):
+def write_text_to_file_with_timestamp(file_path, text, timestamp=False, encoding='utf-8'):
     """
     指定されたファイルパスにテキストを書き込む関数です。timestampがTrueの場合、ファイル名にタイムスタンプを追加します。
 
@@ -75,6 +92,7 @@ def write_text_to_file_with_timestamp(file_path, text, timestamp=False):
     - filepath (str): テキストを書き込むファイルのパス。
     - text (str): ファイルに書き込むテキスト。
     - timestamp (bool, optional): ファイル名にタイムスタンプを追加するかどうか。デフォルトはFalse。
+    - encoding (str): 保存エンコード
     """
     if timestamp:
         # ファイルパスを拡張子前と拡張子に分割
@@ -85,24 +103,40 @@ def write_text_to_file_with_timestamp(file_path, text, timestamp=False):
         file_path = f"{base}{now}{ext}"
 
     # テキストをutf-8でファイルに書き込み
-    with open(file_path, 'w', encoding='utf-8') as file:
+    with open(file_path, 'w', encoding=encoding) as file:
         file.write(text)
 
     # ファイル作成完了のメッセージ
     print(f"ファイル「{file_path}」を作成したよ😊✨！")
 
 
+def load_json_files(files: list[str], n: int, islast: bool = False) -> list[dict]:
+    """
+    指定されたファイルリストから上位(下位)N件のファイルを読み込み、辞書に変換します。
+
+    Args:
+        files (List[str]): ファイルパスのリスト
+        n (int): 読み込むファイルの最大数
+        islast (bool): 下位をとる
+    Returns:
+        list[dict]: 読み込んだJSONファイルの内容を辞書に変換したリスト
+    """
+    top_n_files_content = []
+    for file_path in (sorted(files)[-n:] if islast else sorted(files)[:n]):
+        with open(file_path, 'r', encoding='utf-8') as file:
+            top_n_files_content.append(json.load(file))
+    return top_n_files_content
+
 def send_text_to_gpt(text, session_id):
-    new_message = {"role": "user", "content": f"{text}"}
-    messages = [ {"role": "system", "content": ai_instructions} ]
-    messages.extend(get_message(session_id)[:8])
-    messages.append(new_message)
+    instructions_message = {"role": "system", "content": ai_instructions}
+    messages = get_messsages(session_id, context_length)
+    messages.append({"role": "user", "content": f"{text}"})
     response = client.chat.completions.create(
         model="gpt-4-0125-preview",
-        messages=messages)
+        messages=([instructions_message] + messages)) #pythonでprependってこうやるのがいいらしい [obj] + objs
     if response.choices[0].finish_reason == 'stop':
-        add_message(session_id, new_message)
-        add_message(session_id, response.choices[0].message)
+        messages.append(whitelist_dic(response.choices[0].message.model_dump(), ['role', 'content']))
+        put_messages(session_id, messages[-context_length:]) #最新だけ残すように気を付けること
     return response
 
 # アプリを起動します
